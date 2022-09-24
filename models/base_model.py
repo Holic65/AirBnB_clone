216ff3c7e6a53568ba2f0dc2688b2439e62727fc
Holic65 <okormark@gmail.com>
2022-09-23 19:12:46 -0700

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,38 @@
-def __init__(self):
-        """Initialize a new Base Model
-        """
+#!/usr/bin/python3
+'''
+    Class BaseModel that defines all the common attributes
+    and method for other classes
+'''
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''class initialization of Basemodel'''
+
+    def __init__(self):
+        '''this is the init method'''
         self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation of the Base Model"""
-        return "[{:s}] [{:s}] {}".format(self.__class__.__name__, self.id, self.__dict__)
+        '''A method that describe class'''
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.utcnow()
+        '''updates public instance attribute updated_at'''
+        self.update_at = datetime.now()
 
     def to_dict(self):
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type((self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
+        ''' a method that returns a dic containg k/v pairs'''
+        r = self.__dict__
+        r['__class__'] = self.__class__.__name__
+
+        for key, value in self.__dict__.items():
+            if key in ('created_at', 'updated_at'):
+                r[key] = value.isoformat()
+            else:
+                r[key] = value
+        return r
