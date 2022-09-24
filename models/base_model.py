6244003600a31eabf8b983b57bcdf88ee3919e8e
Holic65 <okormark@gmail.com>
2022-09-23 19:02:44 -0700

    base

--- models/base_model.py
+++ models/base_model.py
@@ -21,18 +21,13 @@ class BaseModel:
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
 
-    def to_dict(self):
-        ''' a method that returns a dic containg k/v pairs'''
-        r = self.__dict__
-        r['__class__'] = self.__class__.__name__
-
-        for key, value in self.__dict__.items():
-            if key in ('created_at', 'updated_at'):
-                r[key] = value.isoformat()
-            else:
-                r[key] = value
-        return r
-
     def save(self):
         ''' updates public instance attribute updated_at'''
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type((self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
