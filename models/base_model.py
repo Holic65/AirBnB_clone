b7eb76ed667447d9c16e7fe6f2cf9c5225dab81d
Holic65 <okormark@gmail.com>
2022-09-29 06:16:09 -0700

    basemodel modified with kqwargs and args

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,28 @@ from datetime import datetime
 class BaseModel:
     '''class initialization of Basemodel'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''this is the init method'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''A method that describe class'''
