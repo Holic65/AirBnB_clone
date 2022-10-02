133e6fe3252c69e1f71dd047b890b67ba57695c1
Holic65 <okormark@gmail.com>
2022-10-01 22:35:22 -0700

    converting name to self

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,30 +15,30 @@ class FileStorage:
 
     def new(self, obj):
         ''' a method that sets object with object keys'''
-        FileStorage.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def all(self):
         ''' a method that returns object stored in obj dict'''
-        return FileStorage.__objects
+        return self.__objects
 
     def save(self):
         ''' a method that serializies object to the json file'''
         dicts = dict()
-        for key, obj in FileStorage.__objects.items():
+        for key, obj in self.__objects.items():
             dicts[key] = obj.to_dict()
 
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
             json.dump(dicts, f)
 
     def reload(self):
         ''' a method that deserializies the json file'''
         if path.exists(self.__file_path) is True:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
                 dicts = json.load(f)
 
             for key in dicts.keys():
                 if BaseModel.__name__ == dicts[key]['__class__']:
-                    FileStorage.__objects[key] = BaseModel(**dicts[key])
+                    self.__objects[key] = BaseModel(**dicts[key])
 
         else:
             with open(self.__file_path, "w", encoding='utf-8') as pathx:
