e6d3ed7ec5c4a7e67bcd5179e2b757a2ab256448
Holic65 <okormark@gmail.com>
2022-10-04 10:00:45 -0700

    import storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@
 '''
 import uuid
 from datetime import datetime
-import models
+import models import storage
 
 
 class BaseModel:
@@ -33,7 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         '''A method that describe class'''
@@ -42,7 +42,7 @@ class BaseModel:
 
     def save(self):
         ''' updates public instance attribute updated_at'''
-        BaseModel.updated_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
